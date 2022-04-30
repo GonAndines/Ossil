83a7d8096bb0700a1e4a30c7f2ce2a4d15be5808
Gonzalo Andines <gon.andines@gmail.com>
2022-04-30 15:02:13 -0300

    primera vers

---
+++ Tesis - Ossil Envases/inc/footer.php
@@ -0,0 +1,42 @@
+<div class="ResForm"></div>
+<div class="ResbeforeSend"></div>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <h4 class="text-footer">Contactanos</h4>
+                
+                    <i class="fa fa-facebook" aria-hidden="true">&nbsp; <a href="http://www.facebook.com/pages/Ossil-Envases/270487806315827?sk=wall" target="_blank">Seguinos en Facebook.</a> </i> 
+                </a><br>
+                
+                    <i class="fa fa-twitter" aria-hidden="true">&nbsp; <a href="https://twitter.com/?lang=es" target="_blank"> Seguinos en Twitter </a> </i>
+                </a><br>
+              
+                    <i class="fa fa-youtube-play" aria-hidden="true">&nbsp; YouTube </i>
+                </a><br>
+               
+                    <i class="fa fa-instagram" aria-hidden="true">&nbsp; Instagram </i>
+                </a><br>
+                
+                    <i class="fa fa-map-marker" aria-hidden="true">&nbsp; Encuentranos </i>
+                </a>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer">Porque elegirnos</h4>
+                <p> 
+                Nuestra premisa es trabajar estrechamente con fabricantes y clientes, para ofrecer soluciones de packaging innovadoras y que aporten valor agregado al producto con los precios más competitivos.</br>
+                </p>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer" >Direccion</h4>
+                <p style="color: #FFF">Oncativo 69
+                <p style="color: #FFF">Córdoba</p>
+                <p style="font-size:20px" aria-hidden="true"> 0351 425-9191</p>
+                <p class="fa fa-mobile" style="font-size:20px"  aria-hidden="true"> 0351 425-9191</p> <a href="#" target="_blank" style="color: #5bc0de">envianos_un_mensaje</a></br>
+                E-mail: <a href="#" target="_blank" style="color: #5bc0de"> &nbsp; ventas@ossilenvases.com.ar</a>
+            </div>
+        </div>
+    </div>
+    <br><br><br>
+    <h5 class="text-center tittles-pages-logo text-footer">Andines - Masanti &copy; </h5>
+</footer>
